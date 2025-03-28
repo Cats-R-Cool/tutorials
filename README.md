d0e92f85a383a3f814283057cebfa263c9bbe2e0
Cats-R-Cool <38488@ma-web.nl>
2025-03-28 12:11:13 +0100

    tutorial2

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ tutorial 0:
 
  tutorial 1:
  ![ezgif-5cc26b797bbe2f](https://github.com/user-attachments/assets/8886fc87-8ad1-4fa5-b95d-b07f218f0b9b)
+
+ tutorial 2:
+ ![alt text](https://s6.ezgif.com/tmp/ezgif-6a664e68ee0dfa.gif)
--- tutorial 0/Assets/scripts/character.cs
+++ tutorial 0/Assets/scripts/character.cs
@@ -16,10 +16,32 @@ public class character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetAxis("Vertical") > 0)
+        {
+         _animator.SetTrigger("walk");
+         _animator.ResetTrigger("idle");
+         _animator.ResetTrigger("dance");
+         _animator.ResetTrigger("walkR");
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+         _animator.SetTrigger("walkR");
+         _animator.ResetTrigger("idle");
+         _animator.ResetTrigger("dance");
+         _animator.ResetTrigger("walk");
+        }
+        else if (Input.GetKey(KeyCode.E))
         {
           _animator.ResetTrigger("idle");
           _animator.SetTrigger("dance");
+          _animator.ResetTrigger("walkR");
+          _animator.ResetTrigger("walk");
+        }
+        else {
+          _animator.SetTrigger("idle");
+          _animator.ResetTrigger("walkR");
+          _animator.ResetTrigger("walk");
+          _animator.ResetTrigger("dance");
         }
     }
 }
